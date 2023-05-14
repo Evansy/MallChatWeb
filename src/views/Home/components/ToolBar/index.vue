4ed8f6930882727f9f35d5add571cc99ab38e114
Evan <skcy@vip.qq.com>
2023-05-14 22:43:43 +0800

    feat: 更新 GitHub 链接

--- src/views/Home/components/ToolBar/index.vue
+++ src/views/Home/components/ToolBar/index.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import { useUserStore } from '@/stores/user'
-// import githubIcon from '@/assets/github.svg'
 const visible = ref(false)
 const userStore = useUserStore()
 
 const avatar = computed(() => userStore?.userInfo.avatar)
-
-const onGithubIconClick = () => {
-  window.open('https://github.com/Evansy/MallChatWeb', '_blank')
-}
 const showSettingBox = () => {
   // 登录了才允许设置个人信息
   userStore.isSign && (visible.value = true)
@@ -24,7 +19,24 @@ const showSettingBox = () => {
       :src="avatar || 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'"
       @click="showSettingBox"
     />
-    <i class="icon-github" @click="onGithubIconClick" />
+    <div class="operate-icons">
+      <a
+        class="operate-icon-link"
+        href="https://github.com/zongzibinbin/MallChat"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="icon-github" /><span class="operate-icon-text">(server)</span>
+      </a>
+      <a
+        class="operate-icon-link"
+        href="https://github.com/Evansy/MallChatWeb"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="icon-github" /><span class="operate-icon-text">(web)</span>
+      </a>
+    </div>
 
     <UserSettingBox v-model="visible" />
   </aside>
