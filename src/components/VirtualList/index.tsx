e5b5e13fea8f4495413938b681344f04485c30d8
李江辰 <SpaceCode@foxmail.com>
2023-06-03 00:21:48 +0800

    feat: 增加虚拟列表、结构重构优化 (#17)
    * feat: 增加虚拟列表、结构重构优化
    
    * chore: 增加注释

---
+++ src/components/VirtualList/index.tsx
@@ -0,0 +1,297 @@
+import { 
+  defineComponent,
+  onActivated,
+  onBeforeMount,
+  onMounted,
+  onUnmounted,
+  ref,
+  watch
+} from 'vue'
+import Virtual from './virtual'
+import Item from './item'
+
+interface Range {
+  start: number
+  end: number
+  padFront: number
+  padBehind: number
+}
+
+interface DataSource {
+    [key: string]: any;
+}
+
+export default defineComponent({
+  name: 'VirtualList',
+  props: {
+    // 数据
+    data: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+    // 唯一标识键值
+    dataKey: {
+      type: [String, Function],
+      required: true,
+    },
+    // 数据项组件
+    item: {
+      type: [Object, Function],
+      required: true,
+    },
+    // 可视区域内保留的数据项个数
+    keeps: {
+      type: Number,
+      default: 30,
+    },
+    size: {
+      type: Number,
+      default: 50,
+    },
+    // 起始索引-用来指定默认从哪里开始渲染
+    start: {
+      type: Number,
+      default: 0,
+    },
+    // 偏移量
+    offset: {
+      type: Number,
+      default: 0,
+    },
+    // 顶部触发阈值
+    topThreshold: {
+      type: Number,
+      default: 0,
+    },
+    // 底部触发阈值
+    bottomThreshold: {
+      type: Number,
+      default: 0,
+    },
+  },
+  setup(props, { emit, expose }) {
+    const range = ref<Range | null>(null)
+    const rootRef = ref<HTMLElement | null>()
+    const shepherd = ref<HTMLDivElement | null>(null)
+    let virtual: Virtual
+
+    // 监听数据数组长度变化 更新数据
+    watch(
+      () => props.data.length,
+      () => {
+        virtual.updateParam('uniqueIds', getUniqueIdFromDataSources())
+        virtual.handleDataSourcesChange()
+      },
+    )
+    watch(
+      () => props.keeps,
+      (newValue) => {
+        virtual.updateParam('keeps', newValue)
+        virtual.handleSlotSizeChange()
+      },
+    )
+    watch(
+      () => props.start,
+      (newValue) => {
+        scrollToIndex(newValue)
+      },
+    )
+    watch(
+      () => props.offset,
+      (newValue) => scrollToOffset(newValue),
+    )
+
+    // 根据id获取数据项大小
+    const getSize = (id: string) => {
+      return virtual.sizes.get(id)
+    }
+    // 获取滚动条偏移量
+    const getOffset = () => {
+        return rootRef.value ? Math.ceil(rootRef.value.scrollTop) : 0
+    }
+    // 获取可视区域大小
+    const getClientSize = () => {
+      const key = 'clientHeight'
+      return rootRef.value ? Math.ceil(rootRef.value[key]) : 0
+    }
+    // 获取滚动条总高度
+    const getScrollSize = () => {
+      const key = 'scrollHeight'
+      return rootRef.value ? Math.ceil(rootRef.value[key]) : 0
+    }
+
+    // 统一处理向外暴露事件
+    const emitEvent = (offset:number, clientSize:number, scrollSize :number) => {
+      emit('scroll', {offset, clientSize, scrollSize})
+      
+      if (virtual.isFront() && !!props.data.length && offset - props.topThreshold <= 0) {
+        emit('totop')
+      } else if (virtual.isBehind() && offset + clientSize + props.bottomThreshold >= scrollSize) {
+        emit('tobottom')
+      }
+    }
+    const onScroll = () => {
+      const offset = getOffset()
+      const clientSize = getClientSize()
+      const scrollSize = getScrollSize()
+      if (offset < 0 || offset + clientSize > scrollSize + 1 || !scrollSize) {
+        return
+      }
+
+      virtual.handleScroll(offset)
+      emitEvent(offset, clientSize, scrollSize)
+    }
+
+    // 获取数据源中的唯一标识
+    const getUniqueIdFromDataSources = () => {
+      const { dataKey, data = [] } = props
+      // 如果dataKey是函数 则调用传入的函数执行获取唯一标识
+      return data.map((dataSource: any) =>
+        typeof dataKey === 'function' ? dataKey(dataSource) : dataSource[dataKey],
+      )
+    }
+    const onRangeChanged = (newRange: any) => {
+      range.value = newRange
+    }
+    /**
+     * 初始化一个virtual实例
+     * @description 详细参数见virtual.ts
+     */
+    const installVirtual = () => {
+      virtual = new Virtual(
+        {
+          slotHeaderSize: 0,
+          slotFooterSize: 0,
+          keeps: props.keeps,
+          estimateSize: props.size,
+          buffer: Math.round(props.keeps / 3),
+          uniqueIds: getUniqueIdFromDataSources(),
+        },
+        onRangeChanged,
+      )
+
+      range.value = virtual.getRange()
+    }
+
+    /**
+     * 滚动到指定索引
+     * @param index 索引值
+     * @description 如果索引值大于等于数据长度说明到底了则滚动到底部
+     */
+    const scrollToIndex = (index: number) => {
+      if (index >= props.data.length - 1) {
+        scrollToBottom()
+      } else {
+        const offset = virtual.getOffset(index)
+        scrollToOffset(offset)
+      }
+    }
+
+    /**
+     * 滚动到指定偏移量
+     * @param offset 滚动条偏移量
+     */
+    const scrollToOffset = (offset: number) => {
+        if (rootRef.value) {
+            rootRef.value.scrollTop = offset
+        }
+    }
+
+    /**
+     * 渲染插槽列表-（重点函数）
+     * @returns {VNode[]} 插槽列表
+     */
+    const getRenderSlots = () => {
+      const slots = []
+      const { start, end } = range.value! // 解构获取范围的起始、结束索引
+      const { data, dataKey, item } = props
+      for (let index = start; index <= end; index++) {
+        const dataSource = data[index] as DataSource // 获取当前索引的数据项
+        if (dataSource) { 
+          // 取这个项里面的唯一标识拿来做key
+          const uniqueKey = typeof dataKey === 'function' ? dataKey(dataSource) : dataSource[dataKey]
+          // 如果唯一标识是字符串或者数字则渲染
+          if (typeof uniqueKey === 'string' || typeof uniqueKey === 'number') {
+            slots.push(
+              <Item
+                index={index}
+                uniqueKey={uniqueKey}
+                source={dataSource}
+                component={item}
+                onItemResize={onItemResized}
+              />,
+            )
+          }
+        }
+      }
+      return slots
+    }
+
+    // 数据项大小改变时触发
+    const onItemResized = (id: string, size: number) => {
+      virtual.saveSize(id, size)
+      emit('resized', id, size)
+    }
+
+    // 滚动到底部
+    const scrollToBottom = () => {
+      if (shepherd.value) {
+        const offset = shepherd.value.offsetTop
+        scrollToOffset(offset)
+        setTimeout(() => {
+          if (getOffset() + getClientSize() < getScrollSize()) {
+            scrollToBottom()
+          }
+        }, 3)
+      }
+    }
+
+    const getSizes = () => {
+      return virtual.sizes.size
+    }
+
+    onBeforeMount(() => {
+      installVirtual()
+    })
+
+    onActivated(() => {
+      scrollToOffset(virtual.offset)
+    })
+
+    onMounted(() => {
+      if (props.start) {
+        scrollToIndex(props.start)
+      } else if (props.offset) {
+        scrollToOffset(props.offset)
+      }
+    })
+
+    onUnmounted(() => {
+      virtual.destroy()
+    })
+
+    expose({
+      scrollToBottom,
+      getSizes,
+      getSize,
+      getOffset,
+      getScrollSize,
+      getClientSize,
+      scrollToOffset,
+      scrollToIndex,
+    })
+
+    return () => {
+      const { padFront, padBehind } = range.value!
+      return (
+        <div ref={rootRef} onScroll={onScroll}>
+          <div style={{padding: `${padFront}px 0px ${padBehind}px`}}>
+            {getRenderSlots()}
+          </div>
+          <div ref={shepherd} style={{ width: '100%', height: '0px' }}/>
+        </div>
+      )
+    }
+  },
+})
---
+++ src/components/VirtualList/virtual.ts
@@ -0,0 +1,318 @@
+/**
+ * @file 虚拟滚动核心逻辑文件 - (构造函数)
+ * @description 主要用于虚拟滚动计算和管理
+ * @param {Object} param 定制参数
+ * @param {Function} callUpdate 回调
+ */
+// @ts-nocheck
+export default class Virtual {
+  sizes: Map<string, number> = new Map<string, number>(); // 存储元素的尺寸的Map对象
+  offset: number;
+  
+  constructor(param, callUpdate) {
+    this.init(param, callUpdate)
+  }
+
+  /**
+   * 初始化
+   * @param {object} param - 参数对象
+   * @param {function} callUpdate - 更新回调函数
+   */
+  init(param, callUpdate) {
+    this.param = param
+    this.callUpdate = callUpdate
+
+    this.sizes = new Map()
+    this.firstRangeTotalSize = 0
+    this.firstRangeAverageSize = 0
+    this.lastCalcIndex = 0
+    this.fixedSizeValue = 0
+    this.calcType = 'INIT'
+
+    this.offset = 0
+    this.direction = ''
+
+    this.range = Object.create(null)
+    if (param) {
+      this.checkRange(0, param.keeps - 1)
+    }
+  }
+
+  // 销毁整个虚拟列表实例
+  destroy() {
+    this.init(null, null)
+  }
+
+  /**
+   * 获取当前范围
+   * @returns {object} - 当前范围对象
+   */
+  getRange() {
+    const range = Object.create(null)
+    range.start = this.range.start
+    range.end = this.range.end
+    range.padFront = this.range.padFront
+    range.padBehind = this.range.padBehind
+    return range
+  }
+
+  isBehind() {
+    return this.direction === 'BEHIND'
+  }
+
+  isFront() {
+    return this.direction === 'FRONT'
+  }
+
+  getOffset(start) {
+    return (start < 1 ? 0 : this.getIndexOffset(start)) + this.param.slotHeaderSize
+  }
+
+  // 更新参数
+  updateParam(key, value) {
+    if (this.param && key in this.param) {
+      if (key === 'uniqueIds') {
+        this.sizes.forEach((v, key) => {
+          if (!value.includes(key)) {
+            this.sizes.delete(key)
+          }
+        })
+      }
+      this.param[key] = value
+    }
+  }
+
+  /**
+   * 保存元素的尺寸
+   * @param {string} id - 唯一标识
+   * @param {number} size - 尺寸
+   * @description 该方法用于保存数据项的尺寸信息。将指定id和尺寸保存到`sizes`映射中，以便后续使用
+   */
+  saveSize(id, size) {
+    this.sizes.set(id, size)
+
+    // 如果当前计算类型为初始化(INIT) 将尺寸值设为固定尺寸值，并将计算类型设置为固定
+    if (this.calcType === 'INIT') {
+      this.fixedSizeValue = size
+      this.calcType = 'FIXED'
+       // 如果当前计算类型固定并且固定尺寸值不等于新的尺寸
+    } else if (this.calcType === 'FIXED' && this.fixedSizeValue !== size) {
+      this.calcType = 'DYNAMIC'
+      delete this.fixedSizeValue // 删除固定尺寸值
+    }
+
+    if (this.calcType !== 'FIXED' && typeof this.firstRangeTotalSize !== 'undefined') {
+      // 如果sizes映射的大小小于参数keeps和uniqueIds长度的最小值 这么做的目的是为了计算平均尺寸
+      if (this.sizes.size < Math.min(this.param.keeps, this.param.uniqueIds.length)) {
+        // 第一个范围的总尺
+        this.firstRangeTotalSize = [...this.sizes.values()].reduce((acc, val) => acc + val, 0)
+        // 第一个范围的平均尺寸
+        this.firstRangeAverageSize = Math.round(this.firstRangeTotalSize / this.sizes.size)
+      } else {
+        delete this.firstRangeTotalSize
+      }
+    }
+  }
+
+  /**
+   * 数据变化处理
+   */
+  handleDataSourcesChange() {
+    let start = this.range.start
+
+    if (this.isFront()) {
+      // 将起始位置向前调整2个单位
+      start = start - 2
+    } else if (this.isBehind()) {
+      // 将起始位置向后调整2个单位
+      start = start + 2
+    }
+
+    start = Math.max(start, 0) // 确保起始位置不小于0
+
+    this.updateRange(this.range.start, this.getEndByStart(start))
+  }
+
+  handleSlotSizeChange() {
+    this.handleDataSourcesChange()
+  }
+
+  /**
+   * 滚动处理
+   * @param offset 偏移量
+   * @description 根据滚动的偏移量判断滚动的方向是向前还是向后
+   */
+  handleScroll(offset) {
+    this.direction = offset < this.offset ? 'FRONT' : 'BEHIND'
+    this.offset = offset
+
+    if (!this.param) {
+      return
+    }
+
+    if (this.direction === 'FRONT') {
+      this.handleFront()
+    } else if (this.direction === 'BEHIND') {
+      this.handleBehind()
+    }
+  }
+
+  /**
+   * 向前滚动处理
+   * @returns {number} 滚动的位置
+   */
+  handleFront() {
+    const overs = this.getScrollOvers()
+    if (overs > this.range.start) {
+      return
+    }
+
+    const start = Math.max(overs - this.param.buffer, 0)
+    this.checkRange(start, this.getEndByStart(start))
+  }
+
+  /**
+   * 向后滚动处理
+   * @returns {number} 滚动的位置
+   */
+  handleBehind() {
+    const overs = this.getScrollOvers()
+    if (overs < this.range.start + this.param.buffer) {
+      return
+    }
+
+    this.checkRange(overs, this.getEndByStart(overs))
+  }
+
+  /**
+   * 获取滚动的位置
+   * @returns {number} 滚动的位置
+   */
+  getScrollOvers() {
+    const offset = this.offset - this.param.slotHeaderSize
+    if (offset <= 0) {
+      return 0
+    }
+
+    if (this.isFixedType()) {
+      return Math.floor(offset / this.fixedSizeValue)
+    }
+
+    let low = 0
+    let middle = 0
+    let middleOffset = 0
+    let high = this.param.uniqueIds.length
+
+    while (low <= high) {
+      middle = low + Math.floor((high - low) / 2)
+      middleOffset = this.getIndexOffset(middle)
+
+      if (middleOffset === offset) {
+        return middle
+      } else if (middleOffset < offset) {
+        low = middle + 1
+      } else if (middleOffset > offset) {
+        high = middle - 1
+      }
+    }
+
+    return low > 0 ? --low : 0
+  }
+
+  /**
+   * 获取指定索引的偏移量
+   * @param {number} givenIndex - 给定的索引
+   * @returns {number} - 偏移量
+   */
+  getIndexOffset(givenIndex) {
+    if (!givenIndex) {
+      return 0
+    }
+
+    let offset = 0
+    let indexSize = 0
+    for (let index = 0; index < givenIndex; index++) {
+      indexSize = this.sizes.get(this.param.uniqueIds[index])
+      offset = offset + (typeof indexSize === 'number' ? indexSize : this.getEstimateSize())
+    }
+
+    this.lastCalcIndex = Math.max(this.lastCalcIndex, givenIndex - 1)
+    this.lastCalcIndex = Math.min(this.lastCalcIndex, this.getLastIndex())
+
+    return offset
+  }
+
+  isFixedType() {
+    return this.calcType === 'FIXED'
+  }
+
+  getLastIndex() {
+    return this.param.uniqueIds.length - 1
+  }
+
+  checkRange(start, end) {
+    const keeps = this.param.keeps
+    const total = this.param.uniqueIds.length
+
+    if (total <= keeps) {
+      start = 0
+      end = this.getLastIndex()
+    } else if (end - start < keeps - 1) {
+      start = end - keeps + 1
+    }
+
+    if (this.range.start !== start) {
+      this.updateRange(start, end)
+    }
+  }
+
+  // 更新范围
+  updateRange(start, end) {
+    this.range.start = start
+    this.range.end = end
+    this.range.padFront = this.getPadFront()
+    this.range.padBehind = this.getPadBehind()
+    this.callUpdate(this.getRange())
+  }
+
+  getEndByStart(start) {
+    const theoryEnd = start + this.param.keeps - 1
+    const truelyEnd = Math.min(theoryEnd, this.getLastIndex())
+    return truelyEnd
+  }
+
+  // 获取前方的预填充大小
+  getPadFront() {
+    if (this.isFixedType()) {
+      return this.fixedSizeValue * this.range.start
+    } else {
+      return this.getIndexOffset(this.range.start)
+    }
+  }
+
+  /**
+   * 获取后方的预填充大小
+   * @returns {number} - 填充大小
+   */
+  getPadBehind() {
+    const end = this.range.end; // 当前显示范围的结束索引
+    const lastIndex = this.getLastIndex(); // 最后一个索引
+
+    // 如果是固定尺寸类型就直接返回固定尺寸的填充大小
+    if (this.isFixedType()) {
+      return (lastIndex - end) * this.fixedSizeValue;
+    }
+
+    // 如果上一次计算的索引等于最后一个索引，则返回当前索引的偏移量减去结束索引的偏移量
+    if (this.lastCalcIndex === lastIndex) {
+      return this.getIndexOffset(lastIndex) - this.getIndexOffset(end);
+    } else {
+      // 否则返回预估尺寸乘以剩余元素个数的填充尺寸
+      return (lastIndex - end) * this.getEstimateSize();
+    }
+  }
+
+  getEstimateSize() {
+    return this.isFixedType() ? this.fixedSizeValue : this.firstRangeAverageSize || this.param.estimateSize
+  }
+}
