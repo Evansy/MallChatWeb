1127ed00d3a7ee8db74cdc2b39149684ec55f5cc
cyh <2419068625@qq.com>
2023-06-05 20:59:52 +0800

    feat: 添加 打包体积分析 and 统一函数书写方式

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,10 @@ import IconsResolver from 'unplugin-icons/resolver'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 const pathSrc = path.resolve(__dirname, 'src')
-
+const lifecycle = process.env.npm_lifecycle_event
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -36,6 +37,8 @@ export default defineConfig({
       dts: path.resolve(pathSrc, 'components.d.ts'),
     }),
     Icons({ autoInstall: true }),
+    // 打包分析
+    lifecycle === 'report' ? visualizer({ open: true, brotliSize: true, filename: 'report.html' }) : null,
   ],
   resolve: {
     alias: {
