0d9feff841a9ab2abcdae838af6f9297bee8c1fe
edram <edram@qq.com>
2023-06-07 12:35:32 +0800

    chore(env): 支持环境多变量配置

--- src/utils/initWorker.ts
+++ src/utils/initWorker.ts
@@ -1,1 +1,1 @@
-export const worker: Worker = new Worker(new URL('./worker.ts', import.meta.url))
+export const worker: Worker = new Worker(new URL('./worker.ts', import.meta.url), { type: 'module' })
