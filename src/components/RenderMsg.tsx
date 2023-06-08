9ccb45b6cf04d71db37824ca834ddea5fbd55690
Evan <skcy@vip.qq.com>
2023-06-09 00:22:55 +0800

    fix: 尝试修复 title xss 注入

--- src/components/RenderMsg.tsx
+++ src/components/RenderMsg.tsx
@@ -1,6 +1,10 @@
 import { defineComponent } from 'vue'
 import DOMPurify from 'dompurify'
 
+// FIXME https://github.com/Evansy/MallChatWeb/issues/45
+// 此方法也是可以被bypass的，完全安全需要修改render方式，不使用v-html
+const sanitize = (text: string) => DOMPurify.sanitize(text, { ALLOWED_TAGS: [], ALLOWED_ATTR: [] })
+
 export default defineComponent({
   props: ['urlMap', 'text', 'isMe'],
   setup(props) {
@@ -8,7 +12,7 @@ export default defineComponent({
       if (!props.urlMap || Object.keys(props.urlMap).length === 0) return props.text
 
       // 先过滤所有标签
-      const clean = DOMPurify.sanitize(props.text, { ALLOWED_TAGS: [], ALLOWED_ATTR: [] })
+      const clean = sanitize(props.text)
 
       // 再替换标签，保证用户输入的内容是干净的
       let result = clean
@@ -17,7 +21,7 @@ export default defineComponent({
           url,
           `<a rel="noopener noreferrer nofollow" target="_blank" class="msg-content-link" style="color: ${
             props.isMe ? '#fff' : 'var(--color-primary)'
-          };" href="${url.includes('http') ? url : `//${url}`}">${title}</a>`,
+          };" href="${url.includes('http') ? url : `//${url}`}">${sanitize(title as string)}</a>`,
         )
       }
       return <div v-html={result} />
