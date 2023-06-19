8d5ba499b21b5be8a97bafe75267a9ef95d9b4ce
Neper <40224384+wangwei2018613@users.noreply.github.com>
2023-06-19 14:58:56 +0800

    feat: 重构消息信息中网址的显示逻辑 (#54)

--- src/components/RenderMsg.tsx
+++ src/components/RenderMsg.tsx
@@ -1,30 +1,42 @@
 import { defineComponent } from 'vue'
-import DOMPurify from 'dompurify'
-
-// FIXME https://github.com/Evansy/MallChatWeb/issues/45
-// 此方法也是可以被bypass的，完全安全需要修改render方式，不使用v-html
-const sanitize = (text: string) => DOMPurify.sanitize(text, { ALLOWED_TAGS: [], ALLOWED_ATTR: [] })
 
 export default defineComponent({
-  props: ['urlMap', 'text', 'isMe'],
+  props: {
+    urlMap: {
+      type: Object,
+      default: () => ({}),
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+    isMe: {
+      type: Boolean,
+      default: false,
+    },
+  },
   setup(props) {
     return () => {
-      if (!props.urlMap || Object.keys(props.urlMap).length === 0) return props.text
-
-      // 先过滤所有标签
-      const clean = sanitize(props.text)
-
-      // 再替换标签，保证用户输入的内容是干净的
-      let result = clean
-      for (const [url, title] of Object.entries(props.urlMap)) {
-        result = result?.replace(
-          url,
-          `<a rel="noopener noreferrer nofollow" target="_blank" class="msg-content-link" style="color: ${
-            props.isMe ? 'var(--color-white)' : 'var(--color-primary)'
-          };" href="${url.includes('http') ? url : `//${url}`}">${sanitize(title as string)}</a>`,
-        )
-      }
-      return <div v-html={result} />
+      // 获取所有匹配的字符串
+      const keys = Object.keys(props.urlMap)
+      // 使用匹配字符串创建动态正则表达式，并将文本拆分为片段数组
+      const fragments = props.text.split(new RegExp(`(${keys.join('|')})`))
+      const color = props.isMe ? '#fff' : 'var(--color-primary)'
+      const chunks = fragments.map((item) =>
+        keys.includes(item) ? (
+          <a
+            rel="noopener noreferrer nofollow"
+            target="_blank"
+            class="msg-content-link"
+            style={{ color }}
+          >
+            {props.urlMap[item]}
+          </a>
+        ) : (
+          item
+        ),
+      )
+      return chunks
     }
   },
 })
