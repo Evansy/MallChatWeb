51505ce08310fe5f001db8266ca21fac056ed5a9
Arsize <1747583980@qq.com>
2023-06-30 17:07:12 +0800

    fix: 修复topbar 错乱,增加横向滚动条 (#69)
    * fix: 修复topbar 错乱,增加横向滚动条
    
    * fix: 去掉style用css复用&优化样式

--- src/views/Home/components/ToolBar/index.vue
+++ src/views/Home/components/ToolBar/index.vue
@@ -14,78 +14,103 @@ const groupStore = useGroupStore()
 const avatar = computed(() => userStore?.userInfo.avatar)
 
 const showSettingBox = () => (visible.value = true)
-const onToMallChat = () => window.open('https://github.com/zongzibinbin/MallChat', '_blank')
-const onToMallChatWeb = () => window.open('https://github.com/Evansy/MallChatWeb', '_blank')
 const toggleGroupListShow = () => (groupStore.showGroupList = !groupStore.showGroupList)
+const operateBtns = [
+  {
+    title: '微信',
+    icon: 'icon-wechat',
+    text: '微信',
+    url: '',
+  },
+  {
+    title: 'bilibili',
+    icon: 'icon-bilibili',
+    text: '',
+    url: 'https://space.bilibili.com/146719540',
+  },
+
+  {
+    title: '语雀',
+    icon: 'icon-yuque',
+    text: '项目文档',
+    url: 'https://www.yuque.com/snab/planet/cef1mcko4fve0ur3',
+  },
+
+  {
+    title: 'tencent cloud',
+    icon: 'icon-tencent-cloud',
+    text: '618超优惠',
+    url: 'https://curl.qcloud.com/qSaH0JLT',
+  },
+  {
+    title: 'MallChatWeb Server',
+    icon: 'icon-github',
+    text: '后端源码',
+    url: '',
+    func: () => window.open('https://github.com/zongzibinbin/MallChat', '_blank'),
+  },
+  {
+    title: 'MallChatWeb Web',
+    icon: 'icon-github',
+    text: '前端源码',
+    url: '',
+    func: () => window.open('https://github.com/Evansy/MallChatWeb', '_blank'),
+  },
+]
 </script>
 
 <template>
   <aside class="side-toolbar">
     <ElAvatar
       size="large"
+      style="min-width: 56px"
       class="side-toolbar-avatar"
       :src="avatar || defaultAvatar"
       v-login="showSettingBox"
     />
-
     <div class="operate-icons">
-      <a
-        class="operate-icon-link"
-        href="https://www.yuque.com/snab/planet/cef1mcko4fve0ur3"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="语雀"
+      <div
+        class="operate-item"
+        v-for="(item, index) in operateBtns"
+        :key="index"
+        :style="client !== 'PC' ? (item.text ? 'min-width:16vw;' : 'min-width:8vw;') : ''"
       >
-        <i class="operate-icon icon-yuque" /><span class="operate-icon-text">项目文档</span>
-      </a>
-      <a
-        class="operate-icon-link"
-        href="https://space.bilibili.com/146719540"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="bilibili"
-      >
-        <i class="operate-icon icon-bilibili" />
-      </a>
+        <div v-if="item.title != '微信'">
+          <a
+            v-if="item.url"
+            class="operate-icon-link"
+            :href="item.url"
+            target="_blank"
+            rel="noopener noreferrer"
+            :title="item.title"
+          >
+            <i :class="`operate-icon ${item.icon}`" /><span class="operate-icon-text">{{
+              item.text
+            }}</span>
+          </a>
+          <a
+            v-else
+            class="operate-icon-link"
+            v-login="item.func"
+            target="_blank"
+            rel="noopener noreferrer"
+            :title="item.title"
+          >
+            <i :class="`operate-icon ${item.icon}`" /><span class="operate-icon-text">{{
+              item.text
+            }}</span>
+          </a>
+        </div>
 
-      <el-tooltip effect="dark" :placement="client === 'PC' ? 'right' : 'bottom'">
-        <template #content>
-          <img class="icon-wechat-qrcode" :src="qrcode" alt="wx qrcode" />
-        </template>
-        <a class="operate-icon-link" target="_blank" rel="noopener noreferrer" title="wechat">
-          <i class="operate-icon icon-wechat" />
-        </a>
-      </el-tooltip>
-
-      <a
-        class="operate-icon-link"
-        href="https://curl.qcloud.com/qSaH0JLT"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="tencent cloud"
-      >
-        <i class="operate-icon icon-tencent-cloud" />
-        <span class="operate-icon-text">618超优惠</span>
-      </a>
-
-      <a
-        class="operate-icon-link"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="MallChatWeb Server"
-        v-login="onToMallChat"
-      >
-        <i class="operate-icon icon-github" /><span class="operate-icon-text">后端源码</span>
-      </a>
-      <a
-        class="operate-icon-link"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="MallChatWeb Web"
-        v-login="onToMallChatWeb"
-      >
-        <i class="operate-icon icon-github" /><span class="operate-icon-text">前端源码</span>
-      </a>
+        <el-tooltip v-else effect="dark" :placement="client === 'PC' ? 'right' : 'bottom'">
+          <template #content>
+            <img class="icon-wechat-qrcode" :src="qrcode" alt="wx qrcode" />
+          </template>
+          <div class="operate-box">
+            <i class="operate-icon icon-wechat" />
+          </div>
+        </el-tooltip>
+      </div>
     </div>
 
     <el-icon class="menu-icon" color="var(--font-main)" :size="32" @click="toggleGroupListShow">
