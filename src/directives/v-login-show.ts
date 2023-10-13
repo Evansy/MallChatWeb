f1ac0c2721a194b2d2c3aa93625221c0174cb364
Evan <skcy@vip.qq.com>
2023-10-14 00:39:25 +0800

    fix: 修复 v-login-show 逻辑判断问题

--- src/directives/v-login-show.ts
+++ src/directives/v-login-show.ts
@@ -4,11 +4,16 @@ import { watchEffect } from 'vue'
 import { useUserStore } from '@/stores/user'
 
 const vLoginShow: Directive = {
-  mounted(el) {
+  mounted(el: HTMLElement) {
+    let display = ''
     const userStore = useUserStore()
+    // 一开始获取到的 isSign 就是 false 如果用原始的 el.remove 等 isSign 变成 true 的时候 el 可无法创建出来
     watchEffect(() => {
       if (!userStore.isSign) {
-        el?.remove()
+        display = el.style.display
+        el.style.display = 'none'
+      } else {
+        el.style.display = display || 'block'
       }
     })
   },
--- src/main.ts
+++ src/main.ts
@@ -21,9 +21,11 @@ const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate) // 数据持久化
 
 const app = createApp(App)
-app.use(pinia).use(router)
-app.directive('login', vLogin) // 登录权限指令-未登录先登录
-app.directive('login-show', vLoginShow) // 登录权限指令-未登录先登录
-app.directive('friends', vFriends) // 是否好友
-app.mount('#app')
+app
+  .use(pinia)
+  .use(router)
+  .directive('login', vLogin) // 登录权限指令-未登录先登录
+  .directive('login-show', vLoginShow) // 登录权限指令-未登录先登录
+  .directive('friends', vFriends) // 是否好友
+  .mount('#app')
 // router.isReady().then(() => app.mount('#app'))
