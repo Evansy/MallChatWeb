0c049350a7da6494cd160d6e6a59de1d061ed7b2
Kkuil <3024067144@qq.com>
2023-10-17 21:36:49 +0800

    feat: 增加显示群主和管理员的功能

--- src/views/Home/Chat/components/UserList/UserItem/index.vue
+++ src/views/Home/Chat/components/UserList/UserItem/index.vue
@@ -5,6 +5,7 @@ import type { UserItem } from '@/services/types'
 import { useUserInfo } from '@/hooks/useCached'
 import { useUserStore } from '@/stores/user'
 import ContextMenu from '../ContextMenu/index.vue'
+import { GROUP_ROLE_MAP } from '@/enums/group'
 const props = defineProps<{ user: UserItem }>()
 const user = toRef(props.user)
 const userInfo = useUserInfo(user.value?.uid)
@@ -41,7 +42,14 @@ const handleRightClick = (e: MouseEvent) => {
       showStatus
       :online="user.activeStatus === OnlineEnum.ONLINE"
     />
-    {{ userInfo.name }}
+      {{ userInfo.name }}
+      <div
+          v-if="GROUP_ROLE_MAP[user.roleId as number]"
+          class="badge flex-center"
+          :class="GROUP_ROLE_MAP[user.roleId as number].class"
+      >
+          {{ GROUP_ROLE_MAP[user.roleId as number].text }}
+      </div>
     <ContextMenu v-model:show="isShowMenu" :options="menuOptions" :uid="(user?.uid as number)" />
   </li>
 </template>
